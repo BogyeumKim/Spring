92f4a4d6454a519f077461b0d28c6310bc924038
username <user@email.mail>
2020-07-28 18:33:56 +0900

    Spring 첫날

---
+++ TestJavaProject/src/test/Printer.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Printer {
+
+	
+	
+	void print() {
+		
+		System.out.println("안녕하세요 ");
+		
+	}
+}
---
+++ TestJavaProject/src/test/PrinterMain.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class PrinterMain {
+
+	public static void main(String[] args) {
+
+		// Spring Container 생성 
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+	
+		
+		Printer printer = (Printer) ctx.getBean("printer");
+		printer.print();
+	}
+
+}
