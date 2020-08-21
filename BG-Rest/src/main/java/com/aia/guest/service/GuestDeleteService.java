745c09d506c162f78ad069307947f27fa7733f97
username <user@email.mail>
2020-08-21 16:54:55 +0900

    수정완료배포

--- BG-Rest/src/main/java/com/aia/guest/service/GuestDeleteService.java
+++ BG-Rest/src/main/java/com/aia/guest/service/GuestDeleteService.java
@@ -1,11 +1,14 @@
 package com.aia.guest.service;
 
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aia.guest.dao.GuestDao;
-import com.aia.guest.model.Guest_book;
 
 @Service
 public class GuestDeleteService {
@@ -16,9 +19,21 @@ public class GuestDeleteService {
 	@Autowired
 	private SqlSessionTemplate template;
 	
-	public int deletePost(int guest_idx) {
-		
+	public int deletePost(int guest_idx, String photo, HttpServletRequest req) {
 		dao=template.getMapper(GuestDao.class);
+		
+		String uri = "/upload";
+		String realPath= req.getSession().getServletContext().getRealPath(uri);
+		
+		File file= new File(realPath,photo);
+		System.out.println("삭제경로주소 :"+file);
+		
+		 if(file.exists()) {
+			 file.delete();
+		 }
+		
+		
+		
 		return dao.deleteIdx(guest_idx);
 	}
 }
